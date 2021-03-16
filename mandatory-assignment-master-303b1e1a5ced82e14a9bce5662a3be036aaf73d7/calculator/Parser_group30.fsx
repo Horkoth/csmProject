9dcf11560718d5f906b6cb5bd7b6b726890ca5e7
Zebzab7 <56087724+Zebzab7@users.noreply.github.com>
2021-03-16 15:52:26 +0100

    updated parser_group

--- mandatory-assignment-master-303b1e1a5ced82e14a9bce5662a3be036aaf73d7/calculator/Parser_group30.fsx
+++ mandatory-assignment-master-303b1e1a5ced82e14a9bce5662a3be036aaf73d7/calculator/Parser_group30.fsx
@@ -11,6 +11,9 @@ open Parser
 #load "Lexer.fs"
 open Lexer
 
+let det = False
+let mutable counter = 0
+
 let rec cleanString input =
   match input with 
     | x::xs when x = '"' -> '\\'::'"'::(cleanString xs)
@@ -20,20 +23,14 @@ let rec cleanString input =
     | _ -> []
 let transform input =
   (String.Concat(Array.ofList((cleanString (Seq.toList input)))))
-  
-let pg input node0 node1 =
-  let mutable counter = 0
-  let rec finished input =
-    match input with
-      | ConditionCmd(x,y) -> Not(x)
-      | Brack(x,y) -> And((finished x),(finished y))
-  let rec helper input node0 node1 =
+
+let rec helper input node0 node1 =
     //guarded commands
     match input with
-      | ConditionCmd(x,y)   -> counter <- counter + 1
+      | ConditionCmd(x,y)   -> counter := counter + 1
                                "q" + (string node0) + " -> q" + (string counter) + " [label = \"" + (transform (string x)) + "\"];\n" + (edges y (string counter) node1)
       | Brack(x,y)          -> (helper x node0 node1) + (helper y node0 node1)
-  and edges input node0 node1 =
+and edges input node0 node1 =
     //commands
     match input with
       | VarAssignCmd(x,y)   -> "q" + (string node0) + " -> q" + (string node1) + " [label = \"" + (transform (string input)) + "\"];\n"
@@ -41,9 +38,39 @@ let pg input node0 node1 =
       | IfCmd(x)            -> helper x node0 node1
       | DoCmd(x)            -> "q" + (string node0) + " -> q" + (string node1) + " [label = \"" + (string (finished x)) + "\"];\n" + (helper x node0 node0)
       | Skip(x)             -> "q" + (string node0) + " -> q" + (string node1) + " [label = \"" + (transform (string input)) + "\"];\n"
-      | Scolon(x,y)         -> counter <- counter + 1
+      | Scolon(x,y)         -> counter := counter + 1
                                (edges x node0 (string counter)) + (edges y (string counter) node1)
-  "digraph program_graph {rankdir=LR;\nnode [shape = circle]; q▷;\nnode [shape = doublecircle]; q◀;\nnode [shape = circle]\n" + (edges input node0 node1) + "}"
+
+let non_det_pg input node0 node1 =
+  
+
+let det_pg input node0 node1 det =
+  let mutable counter = 0
+
+  let rec finished input =
+    match input with
+      | ConditionCmd(x,y) -> Not(x)
+      | Brack(x,y) -> And((finished x),(finished y))
+  let rec cmd input node0 node1 =
+    match input with
+    | IfCmd(x) -> let (E,d) = gc x node0 node1 False
+                  E
+    | DoCmd(x) -> let (E,d) = gc x node0 node0 False
+                  E + ("q" + (string node0) + " -> q" + (string node1) + " [label = \"" + " Not (" + d + ")" + "\"];\n")
+    | x -> edges x node0 node1
+  and gc input node0 node1 d =
+    match input with
+    | Brack(x,y) -> let (E1,d1) = gc x node0 node1 d
+                    let (E2,d2) = gc y node0 node1 d1
+                    (E1 + E2, d2)
+    | ConditionCmd(b,C) -> counter := counter + 1
+                           let E = edges C counter node1
+                           ("q" + (string node0) + " -> q" + (string counter) + " [label = \"" + " And (b, Not (d)) " + "\"];\n" + E, "Or (b, " + d + ")")
+  
+  if det then
+    "digraph program_graph {rankdir=LR;\nnode [shape = circle]; q▷;\nnode [shape = doublecircle]; q◀;\nnode [shape = circle]\n" + (cmd input node0 node1) + "}"
+  else
+    "digraph program_graph {rankdir=LR;\nnode [shape = circle]; q▷;\nnode [shape = doublecircle]; q◀;\nnode [shape = circle]\n" + (edges input node0 node1) + "}"
 
 // We
 let parse input =
@@ -60,7 +87,11 @@ let rec compute =
   
   let e = parse (Console.ReadLine())
 
-  printfn "\n%s" (pg e "▷" "◀")
+  if det then
+    printfn "\n%s" (det_pg e "▷" "◀")
+  else 
+    printfn "\n%s" (non_det_pg e "▷" "◀")
+    
   printfn "\n%A\n" e
 
 // Start interacting with the user
